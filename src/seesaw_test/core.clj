636a8c718b5b063d5fa782b4354e77aa162f13eb
paul martin <mussym@live.com>
2019-08-30 11:02:53 -0500

    adding buttons and menus

--- src/seesaw_test/core.clj
+++ src/seesaw_test/core.clj
@@ -1,22 +1,50 @@
+
 (ns seesaw-test.core
   (:gen-class)
   (:use seesaw.core)
   (:use seesaw.mig)
-)
+  )
 
 
 (defn refresh-action-handler [e]
   (println "handler invoked"))
 
+(defn quit-action-handler [e]
+  (println "quit")
+  (System/exit 0))
+
+
 (def refresh-action
   (action :name "Refresh" :key "menu R" :handler refresh-action-handler))
 
+(def quit-action
+  (action :name "Quit" :key "menu Q" :handler quit-action-handler))
 
 (defn add-behaviours
   [root]
   
-(config! (select root [:.refresh]) :action refresh-action)
+  (config! (select root [:.refresh]) :action refresh-action)
+  (config! (select root [:.quit]) :action quit-action)
   root)
+
+(defn make-button
+  [caption]
+  (button :text caption)
+  )
+
+(defn make-text-multi
+  [caption]
+  (text :multi-line? true))
+
+(defn make-items
+  []
+  [
+   ["name:"] [(text)]
+   ["age:"] [(text)]
+   ["button:"] [(make-button "fred")]
+   ["docs:"] [(make-text-multi "fred")]
+   ] )
+
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -26,14 +54,15 @@
   (invoke-later
    (-> (frame :title "Hello"
               :size [600 :by 600]
-              :content (mig-panel :constraints ["wrap2" "[shrink 0]20px[200, grow, fill]" "[shrink 0]5px[]"]
-                                  :items [ ["name:"] [(text)]
-                                          ["age:"] [(text)]])
+              :content (mig-panel
+                        :constraints ["wrap2" "[shrink 0]20px[200, grow, fill]" "[shrink 0]5px[]5px[]20px[grow, fill]"]
+                        :items (make-items)
+                        )
               :on-close :exit
               :menubar
               (menubar :items
-                       [(menu :text "File" :items [(menu-item :class :refresh)])
-                        (menu :text "Edit" :items [(menu-item :class :refresh)])]))
+                       [(menu :text "File" :items [(menu-item :class :refresh) (menu-item :class :quit)])
+                        (menu :text "Edit" :items [(menu-item :class :quit)])]))
        add-behaviours
        show!)
    ))
